188ba64ccc7aee100b5e1318411b714b89e1f763
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-11-04 23:35:42 -0300

    menu

--- src/ComponentesConstantes/Interface/index.js
+++ src/ComponentesConstantes/Interface/index.js
@@ -1,10 +1,31 @@
-
+import "./style.css";
+//import Imagens from './AmiMais\src\ComponentesConstantes\Imagens\amimaisLogo.png';
 
 export function Interface () {
    
-   return (
+   return ( 
  <>
-    <h2>Hello world</h2>
+     
+   <header>
+ <div className = 'imgMenu'> 
+   <img src="https://media.discordapp.net/attachments/834862603792285717/1037725076126179328/amimaisLogo.png" />
+</div>     
+
+      <div className = 'interface'> 
+         <a href="#">Inicio </a> 
+         <a href="#">Sobre nós </a>
+         <a href="#">Resgate</a>
+         <a href="#">Curiosidades </a> 
+         <a href="#">Login </a> 
+         <a href="#">Cadastrar </a>
+
+         <div className="perfilLateral">
+        
+        </div>
+      </div> 
+      
+   
+   </header>
  </>
    )
 }
