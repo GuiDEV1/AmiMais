bb9004df7d7e28aa9cdb5792e4e00815cb795295
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-11-25 13:33:12 -0300

    inicio dos maus tratos

--- src/Pages/MausTratos/index.js
+++ src/Pages/MausTratos/index.js
@@ -1,5 +1,9 @@
 import { Interface } from "../../ComponentesConstantes/Interface";
-
+import "./style.css";
+import mausTratos1 from "./Imagens/Reporta2_2.png";
+import mausTratos2 from "./Imagens/MausTratos2.png";
+import { Rodape } from "../../ComponentesConstantes/Rodape";
+import Girl from "./Imagens/HappyGirl.png";
 
 export function MausTratos () {
    
@@ -8,17 +12,41 @@ export function MausTratos () {
     <Interface />
    <div id="MausTratos">
 
-      <h1>Maus Tratos</h1>
+      <h1>Maus <strong className="scd">Tratos</strong></h1>
       <div id="headerimg">
-         <img src="" alt="headerimg" />
-         <img src="" alt="headerimg" />
+      <img src={mausTratos2} alt="headerimg" />
+      <img src={mausTratos1} alt="headerimg" />
       </div>
-      <h2>Denuncie</h2>
-       <div id="footerimg">
-          <div id="Conteudo1">Conteudo1</div>
-          <div id="Conteudo2">conteudo2</div>
+      <h2>Denu<strong className="scd">ncie</strong></h2>
+
+       <div id="footerPai">
+
+          <div id="Conteudo1">
+            <img src={Girl} alt="Uma moça sorridente"/>
+
+          <p>
+            Denuncie ao orgão responsavel por maus trato de animais , 
+            sua atitude salva vidas , ao aperta o botão logo abaixo 
+            será redrecionado para o orgão responsavel .
+          </p>
+          </div>
+
+          <div id="Conteudo2">
+              <div>
+                  <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Quisque sed venenatis urna, in porta enim. Quisque tristique, 
+                  orci vel  <br/><br/>
+
+                  commodo rutrum, sapien elit eleifend lacus, euismod fermentum 
+                  erat ex eget lacus. Aliquam ornare nec quam id scxcs 
+                </p>   
+              </div>
+              <button>Denunciar</button>
+          </div>
        </div>
   </div> 
+  <Rodape/>
  </>
    )
 }
